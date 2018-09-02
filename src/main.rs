a906e019da673332b393c02758e6c346b932d5b7
Joshua Li <josh@jrl.ninja>
2018-09-01 23:22:51 -0400

    mirror stdin to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate rand;
 extern crate getopts;
 
-use std::{env, char};
+use std::{env, io, char};
 use rand::{thread_rng, Rng};
 use getopts::Options;
 
@@ -22,14 +22,26 @@ fn main() {
     opts.optopt("k", "kana", "append N random katakana characters, up to 255", "N");
 
     let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!(f.to_string()) }
+        Ok(m) => m,
+        Err(e) => panic!(e.to_string()),
     };
 
     if matches.opt_present("h") {
         print_usage(&program, opts);
         return;
     }
+
+    let mut input = String::new();
+    loop {
+        let bytes_read = match io::stdin().read_line(&mut input) {
+            Ok(n) => n,
+            Err(e) => panic!(e.to_string()),
+        };
+        if bytes_read == 0 {
+            break;  // indicates EOF
+        }
+    }
+    print!("{}", input);
 
     let kata_opt = matches.opt_str("k");
     let mut num_kata: u8 = match kata_opt {
@@ -43,8 +55,8 @@ fn main() {
         while num_kata > 0 {
             let n: u32 = rng.gen_range(KANA_LO, KANA_HI + 1);
             let c = match char::from_u32(n) {
-                Some(x) => { x }
-                None => { '\0' } // lol
+                Some(x) => x,
+                None => '\0', // lol
             };
             print!("{}", c);
             num_kata -= 1;
